24eaeae57ce20c4f699b949bba6f9861928669c5
m.lucas <m.lucas@groupeonepoint.com>
2017-10-26 10:35:13 +0200

    Ajout d'animations

---
+++ FullRandom.py
@@ -0,0 +1,18 @@
+import random
+
+from arbalet.colors import hsv_to_rgb
+from arbalet.core import Arbalet
+from arbaletApp import ArbaletApp
+
+
+class FullRandom(ArbaletApp):
+
+    #Affiche une couleur random sur chaque case (raffraichit a chaque boucle)
+    def loop(self, arbalet: Arbalet):
+        for i in range(arbalet.height):
+            for j in range(arbalet.width):
+                color = (random.random(), random.random(), random.random())
+                arbalet.user_model.set_pixel(i, j, color)
+
+
+FullRandom().start()
